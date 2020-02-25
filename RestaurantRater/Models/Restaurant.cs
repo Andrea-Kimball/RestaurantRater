e222b2f98b729847d461f4ceab43642348707c64
Andrea Kimball <aundreya.7@gmail.com>
2020-02-25 10:44:38 -0500

    Add project files.

---
+++ RestaurantRater/Models/Restaurant.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.Models
+{
+    public class Restaurant
+    {
+        //Primary key is the first item in the list unless otherwise defined
+        //look up other annotations
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Style { get; set; }
+        [Required]
+        [Range(0d, 5d)]
+        public string Name { get; set; }
+        [Required]
+        public double Rating { get; set; }
+        [Required]
+        [Range(1,5)]
+        public int DollaSigns { get; set; }
+    }
+}
---
+++ RestaurantRater/Models/RestaurantDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.Models
+{
+    //using entity framework we have inherited everything we need from Db Context
+    public class RestaurantDbContext : DbContext
+    {
+        public RestaurantDbContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Restaurant> Restauranst { get; set; }
+
+    }
+}
